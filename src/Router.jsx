8dad2503c1c237abc70bedaa8cb286f2a2c590f6
hdito <egorermolaef@gmail.com>
2022-05-03 22:00:09 +0300

    First deploy

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Test } from "./Test.jsx";
 import App from "./App.jsx";
+import { useEffect } from "react";
 
 export function Router() {
   return (
@@ -10,6 +11,14 @@ export function Router() {
           <Route path="test1" element={<Test title="Test 1" />} />
           <Route path="test2" element={<Test title="Test 2" />} />
           <Route path="test3" element={<Test title="Test 3" />} />
+          <Route
+            path="*"
+            element={
+              <main>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/Test.jsx
+++ src/Test.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from "react";
 
 export function Test({ title }) {
-  useEffect(() => {
-    console.log(title);
-  });
   return <h1>{title}</h1>;
 }
